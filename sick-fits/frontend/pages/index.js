62487afb414b2074fc8345abd5d0dc8c7d6020c3
hughdevore <hughdevore@gmail.com>
2018-12-17 15:03:15 -0700

    Completed Module 1.

---
+++ sick-fits/frontend/pages/index.js
@@ -0,0 +1,10 @@
+import {Fragment} from 'react';
+import Link from 'next/link';
+
+const Home = props => (
+    <Fragment>
+        <p>Howdy!</p>
+    </Fragment>
+);
+
+export default Home;
---
+++ sick-fits/frontend/pages/sell.js
@@ -0,0 +1,10 @@
+import {Fragment} from 'react';
+import Link from 'next/link';
+
+const Sell = props => (
+    <Fragment>
+        <p>Sell!</p>
+    </Fragment>
+);
+
+export default Sell;
